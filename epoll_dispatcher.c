27346ef5f467737dc1c20b306961f490182f0510
wen2ms <1819509111@qq.com>
2025-04-29 17:20:21 +0800

    Feat: define epoll_add, epoll_remove and epoll_modify

--- epoll_dispatcher.c
+++ epoll_dispatcher.c
@@ -16,6 +16,7 @@ static int epoll_remove(struct Channel* channel, struct EventLoop* evloop);
 static int epoll_modify(struct Channel* channel, struct EventLoop* evloop);
 static int epoll_dispatch(struct EventLoop* evloop, int timeout);
 static int epoll_clear(struct EventLoop* evloop);
+static int epoll_op(struct Channel* channel, struct EventLoop* evloop, int op);
 
 struct Dispatcher epoll_dispatcher = {epoll_init, epoll_add, epoll_remove, epoll_modify, epoll_dispatch, epoll_clear};
 
@@ -31,11 +32,49 @@ static void* epoll_init() {
     return data;
 }
 
-static int epoll_add(struct Channel* channel, struct EventLoop* evloop) {}
+static int epoll_op(struct Channel* channel, struct EventLoop* evloop, int op) {
+    struct EpollData* data = (struct EpollData*)evloop->dispatcher_data;
+    struct epoll_event ev;
+    ev.data.fd = channel->fd;
 
-static int epoll_remove(struct Channel* channel, struct EventLoop* evloop) {}
+    int events = 0;
+    if (channel->events & kReadEvent) {
+        events |= EPOLLIN;
+    }
+    if (channel->events & kWriteEvent) {
+        events |= EPOLLOUT;
+    }
+    ev.events = events;
+    int ret = epoll_ctl(data->epfd, op, channel->fd, &ev);
+    return ret;
+}
 
-static int epoll_modify(struct Channel* channel, struct EventLoop* evloop) {}
+static int epoll_add(struct Channel* channel, struct EventLoop* evloop) {
+    int ret = epoll_op(channel, evloop, EPOLL_CTL_ADD);
+    if (ret == -1) {
+        perror("epoll_add");
+        exit(0);
+    }
+    return ret;
+}
+
+static int epoll_remove(struct Channel* channel, struct EventLoop* evloop) {
+    int ret = epoll_op(channel, evloop, EPOLL_CTL_DEL);
+    if (ret == -1) {
+        perror("epoll_remove");
+        exit(0);
+    }
+    return ret;
+}
+
+static int epoll_modify(struct Channel* channel, struct EventLoop* evloop) {
+    int ret = epoll_op(channel, evloop, EPOLL_CTL_MOD);
+    if (ret == -1) {
+        perror("epoll_modify");
+        exit(0);
+    }
+    return ret;
+}
 
 static int epoll_dispatch(struct EventLoop* evloop, int timeout) {}
 
