eb2dbc4947f060c6a7e9399f3e692d0e481bca0b
wen2ms <1819509111@qq.com>
2025-04-29 11:07:37 +0800

    Feat: add Channel module

---
+++ channel.c
@@ -0,0 +1,25 @@
+#include "channel.h"
+
+#include <stdlib.h>
+
+struct Channel* channel_init(int fd, int events, handle_func read_func, handle_func write_func, void* arg) {
+    struct Channel* channel = (struct Channel*)malloc(sizeof(struct Channel));
+    channel->fd = fd;
+    channel->events = events;
+    channel->read_callback = read_func;    
+    channel->write_callback = write_func;
+    channel->arg = arg;
+    return channel;
+}
+
+void write_event_enable(struct Channel* channel, bool flag) {
+    if (flag) {
+        channel->events |= kWriteEvent;
+    } else {
+        channel->events &= ~kWriteEvent;
+    }
+}
+
+bool is_write_event_enable(struct Channel* channel) {
+    return channel->events & kWriteEvent;
+}
---
+++ channel.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef int (*handle_func)(void* arg);
+
+enum FDEvent {
+    kTimeout = 0x01,
+    kReadEvent = 0x02,
+    kWriteEvent = 0x04
+};
+
+struct Channel {
+    int fd;
+    int events;
+    handle_func read_callback;
+    handle_func write_callback;
+    void* arg;
+};
+
+struct Channel* channel_init(int fd, int events, handle_func read_func, handle_func write_func, void* arg);
+void write_event_enable(struct Channel* channel, bool flag);
+bool is_write_event_enable(struct Channel* channel);
