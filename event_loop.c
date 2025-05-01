5008cb12ee3365f1096240fb3a1a9229f853f97b
wen2ms <1819509111@qq.com>
2025-05-01 19:51:55 +0800

    Feat: EventLoop init

---
+++ event_loop.c
@@ -0,0 +1,22 @@
+#include "event_loop.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+struct EventLoop* event_loop_init() {
+    return event_loop_init_ex(NULL);
+}
+
+struct EventLoop* event_loop_init_ex(const char* thread_name) {
+    struct EventLoop* ev_loop = (struct EventLoop*)malloc(sizeof(struct EventLoop));
+    ev_loop->is_quit = false;
+    ev_loop->thread_id = pthread_self();
+    pthread_mutex_init(&ev_loop->mutex, NULL);
+    strcpy(ev_loop->thread_name, thread_name == NULL ? "main_thread" : thread_name);
+    ev_loop->dispatcher = &epoll_dispatcher;
+    ev_loop->dispatcher_data = ev_loop->dispatcher->init();
+    ev_loop->head = ev_loop->tail = NULL;
+    ev_loop->channel_map = channel_map_init(128);
+
+    return ev_loop;
+}
--- event_loop.h
+++ event_loop.h
@@ -1,9 +1,39 @@
 #pragma once
 
+#include <stdbool.h>
+#include <pthread.h>
+
 #include "dispatcher.h"
+#include "channel.h"
+#include "channel_map.h"
 
 extern struct Dispatcher epoll_dispatcher;
+extern struct Dispatcher poll_dispatcher;
+extern struct Dispatcher select_dispatcher;
+
+enum ElemType {
+    kAdd,
+    kDelete,
+    kModify
+};
+
+struct ChannelElement {
+    int type;
+    struct Channel* channel;
+    struct ChannelElement* next;
+};
+
 struct EventLoop {
+    bool is_quit;
     struct Dispatcher* dispatcher;
     void* dispatcher_data;
+    struct ChannelElement* head;
+    struct ChannelElement* tail;
+    struct ChannelMap* channel_map;
+    pthread_t thread_id;
+    char thread_name[32];
+    pthread_mutex_t mutex;
 };
+
+struct EventLoop* event_loop_init();
+struct EventLoop* event_loop_init_ex(const char* thread_name);
